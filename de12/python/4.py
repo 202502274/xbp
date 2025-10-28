950cffdc3778b65c2c504ba45abc5737325ea26a
202502274 <r202502274bp@jindai.jp>
2025-10-28 16:51:18 +0900

    a

---
+++ de12/python/4.py
@@ -0,0 +1,7 @@
+#if for 変数
+
+for i in range(1,6):
+    list=[1,2,3,4,5,6]
+    import random
+    print(random.choice(list))
+    list=[(random.choice(list))]
--- de12/python/main.py
+++ de12/python/main.py
@@ -1,7 +1,7 @@
-#以下、変数の代入
-name="あああ"  #""によって文字と認識される
-waist=60
-age=30
+for i in range(1,6):
+    dice_list=[1,2,3,4,5,6]
+    import random
+    kekka_list=[(random.choice(dice_list))]
+    print(kekka_list)
 
-#print...出力の命令
-print(name, "さんは腹囲", waist, "cmで年齢は",age, "才ですね。")
+
