d942d2d1a980f26bd1a552b421c8e6421d213226
202502274 <r202502274bp@jindai.jp>
2025-11-04 13:56:36 +0900

    20251104

--- de12/python/work2.py
+++ de12/python/work2.py
@@ -1,14 +1,12 @@
-name=input("名前を教えてください")
-waist=float(input("腹囲は？"))
-age=int(input("年齢は？"))
+for i in range(1,4): #0から数える、(以上,未満)
+    print(i,"人目") #タブでずらしていることに注意
+    name=input("名前を教えてください")
+    waist=float(input("腹囲は？"))
+    age=int(input("年齢は？"))
 
-print(name, "さんは腹囲", waist, "cmで年齢は",age, "才ですね。")
+    print(name, "さんは腹囲", waist, "cmで年齢は",age, "才ですね。")
 
-if waist>=85 and age>=40:
-    print(name,"さん、内臓脂肪蓄積注意です")
-else:
-    print(name,"さん、腹囲は問題ありません")
-
-#if...条件分岐
-#int...整数、float...小数対応
-#str...文字
+    if waist>=85 and age>=40:
+        print(name,"さん、内臓脂肪蓄積注意です")
+    else:
+        print(name,"さん、腹囲は問題ありません")
