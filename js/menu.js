02a145c2694cb954dac7743f29336345b9cc60b9
202502274 <r202502274bp@jindai.jp>
2026-04-13 18:24:08 +0900

    202604133

--- js/menu.js
+++ js/menu.js
@@ -5,28 +5,16 @@ function isMobile() {
   return window.innerWidth <= 768;
 }
 
-// ハンバーガー
 hamburger.addEventListener("click", () => {
   if (isMobile()) {
     menu.classList.toggle("active");
   }
 });
 
-// サブメニュー
 document.querySelectorAll(".has-sub > span").forEach(item => {
   item.addEventListener("click", () => {
     if (isMobile()) {
       item.nextElementSibling.classList.toggle("active");
     }
   });
-});
-
-// PCに戻ったときリセット
-window.addEventListener("resize", () => {
-  if (!isMobile()) {
-    menu.classList.remove("active");
-    document.querySelectorAll(".dropdown").forEach(d => {
-      d.classList.remove("active");
-    });
-  }
 });
